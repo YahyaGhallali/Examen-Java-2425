d45c8df0bf3ae6d9ad601bcc28eabdd4a85f0358
Yahya Ghallali <Yahyaghallali88@gmail.com>
2025-01-06 08:29:15 +0100

    Readme update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Examen Java 2024/25
+
+## 1. Diagramme de classe:
+```mermaid
+```
+
+## 2. Diagramme MLD
+
+## 3. Tables MySQL
+
+```mysql
+```
+
+## 4. 
+## 5.  
+## 6. 
+## 7. 
+## 8. 
+## 9. 
+## 10. 
+## 11. 
+## 12. 
--- src/main/java/com/mri/examenjava2425/MainController.java
+++ src/main/java/com/mri/examenjava2425/MainController.java
@@ -4,11 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome Examen Java!");
+        System.out.println("Hello, JavaFX!");
     }
 }
